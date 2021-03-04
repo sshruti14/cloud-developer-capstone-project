d21d69eb63c7af1ef46a3e6e5d17a582352c64f0
sshruti14 <shetty_14august@yahoo.com>
2021-03-04 18:59:41 -0500

    Commiting the files for cat lovers app

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'rjzihcqm78'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-1raa98x9.us.auth0.com',
+  clientId: 'vQwqv9OQxe9pE0WBSSI3ITQmmP6ynDCf',
+  callbackUrl: 'http://localhost:3000/callback'
+};
